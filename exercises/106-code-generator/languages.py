2346d9fa5d575d566ebe874fec3598af4f3b386f
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2018-01-16 21:09:35 -0200

    better abstraction

--- exercises/106-code-generator/languages.py
+++ exercises/106-code-generator/languages.py
@@ -1,33 +1,106 @@
-DEFAULTS = {
-        }
+'''Deals with language tools for data conversion, code generation and such.'''
+import re
 
-templates = {
-        'cpp': {
-            'line-comment': '//{comment}',
-            'type-def': {
-                'begin': 'class {type_name} {{',
-                'field': '{field_type} {field_name};',
-                'end': '}};',
-                },
-            'types': {
-                'str': 'std::string',
-                'char': 'char',
-                'array': 'std::array<{inner_type}, {size}>',
-                'vector': 'std::vector<{inner_type}>',
-                },
-            },
-        'py': {
-            'line-comment': '#{comment}',
-            'type-def': {
-                'begin': 'class {type_name}:',
-                'field': '{field_name}: {field_type}',
-                'end': '',
-                },
-            'types': {
-                'str': 'str',
-                'char': 'str',
-                'array': 'List[{inner_type}]',
-                'vector': 'List[{inner_type}]',
-                }
+from typing import List, NamedTuple
+
+
+class Comment(NamedTuple):
+    text: str = ''
+
+
+class Array(NamedTuple):
+    inner_type: str
+    length: int = -1
+
+
+class Field(NamedTuple):
+    name: str
+    value_type: str
+    comment: Comment = Comment('')
+
+
+class DataType(NamedTuple):
+    name: str
+    fields: List[Field]
+
+
+class Parser:
+    def __init__(self):
+        self.comment = Comment()
+        self.types = []
+
+
+def parse(text: str):
+    def next_line(lines):
+        return next(lines).split('#', maxsplit=1)
+
+    def begin_type(lines, name, suffix=''):
+        return DataType(name=f'{name}{suffix}', fields=parse_fields(lines))
+
+    def parse_fields(lines):
+        def extract_type(text):
+            def as_array(field_type):
+                match = re.compile('([a-zA-Z0-9]+)\[(\d*)\]').match(
+                                   field_type)
+                if match:
+                    return Array(*match.groups())
+                return None
+
+            return (as_array(text) or
+                    text)
+
+        fields = []
+        while True:
+            line = next_line(lines)[0].split()
+            command = line[0]
+            if command == 'E':
+                return fields
+            elif command == 'F':
+                field_name, field_type = line[1:]
+                fields.append(Field(name=field_name,
+                                    value_type=extract_type(field_type)))
+
+    def common_parse(lines):
+        while True:
+            line = next_line(lines)
+            if len(line) == 0:
+                continue
+            if len(line) == 2:
+                data = line[0].split()
+                comment = line[1]
+            else:
+                data = line[0].split()
+                comment = None
+
+            if len(data) > 0:
+                command = data[0]
+                if command == 'M':
+                    yield begin_type(lines, data[1], suffix='Msg')
+            if comment:
+                yield Comment(comment)
+    return [*common_parse(iter(text.splitlines()))]
+
+
+def generate_cpp(elements):
+    def generate_type(data):
+        texts = [f'class {data.name} {{']
+        for field in data.fields:
+            name, value_type, comment = field
+            if isinstance(value_type, Array):
+                value_type = f'std::vector<{value_type.inner_type}>'
+            texts.append(f'    {value_type} {name} {generate_comment(comment)}')
+        texts.append('};')
+        return '\n'.join(texts)
+
+    def generate_comment(comment):
+        return f'//{comment.text}' if len(comment.text) > 0 else ''
+
+    ELM_GENERATORS = {
+            DataType: generate_type,
+            Comment: generate_comment,
             }
-        }
+    return '\n'.join([ELM_GENERATORS[type(elm)](elm) for elm in elements])
+
+
+def generate_py(elms):
+    print('not yet')
--- exercises/106-code-generator/transaction.py
+++ exercises/106-code-generator/transaction.py
@@ -1,81 +1,7 @@
 '''Database transaction module.'''
-import re
-
 from carl import command
 
-from languages import templates as lang_templates
-
-
-def parse(text, lang):
-    print(f'generating for {lang}:')
-    lang = lang_templates[lang]
-
-    def next_line(lines):
-        return next(lines).split('#', maxsplit=1)
-
-    def begin_type(lines, name, suffix=''):
-        type_def = {
-                'name': f'{name}{suffix}',
-                'fields': {},
-                }
-        return '\n'.join([
-            lang['type-def']['begin'].format(type_name=type_def['name']),
-            *parse_fields(lines, type_def),
-            lang['type-def']['end'].format(type_name=type_def['name'])])
-
-    def parse_fields(lines, type_def):
-        def format_field(field_type):
-            def to_array(field_type):
-                match = re.compile('([a-zA-Z0-9]+)\[(\d*)\]').match(
-                                   field_type)
-                if match:
-                    return match.groups()
-                return None
-
-            array = to_array(field_type)
-            if array:
-                inner_type = lang['types'][array[0]]
-                if len(array[1]) > 0:
-                    size = int(array[1])
-                    return lang['types']['array'].format(
-                            inner_type=inner_type,
-                            size=size)
-                return lang['types']['vector'].format(
-                        inner_type=inner_type)
-
-            if field_type not in lang['types']:
-                return field_type
-        while True:
-            line = next_line(lines)[0].split()
-            command = line[0]
-            if command == 'E':
-                return
-            elif command == 'F':
-                field_name, field_type = line[1:]
-                type_def['fields'][field_name] = {'type': field_type}
-                yield ' '*4 + lang['type-def']['field'].format(
-                    field_name=field_name,
-                    field_type=format_field(field_type))
-
-    def common_parse(lines):
-        while True:
-            line = next_line(lines)
-            if len(line) == 0:
-                continue
-            if len(line) == 2:
-                data = line[0].split()
-                comment = line[1]
-            else:
-                data = line[0].split()
-                comment = None
-
-            if len(data) > 0:
-                command = data[0]
-                if command == 'M':
-                    yield begin_type(lines, data[1], suffix='Msg')
-            if comment:
-                yield lang['line-comment'].format(comment=comment)
-    return '\n'.join([*common_parse(iter(text.splitlines()))])
+from languages import generate_cpp, generate_py, parse
 
 
 @command
@@ -86,11 +12,16 @@ def main():
 @main.subcommand
 def gen(input: str,
         languages: str='cpp,py'):
+    GENERATORS = {
+            'cpp': generate_cpp,
+            'py': generate_py,
+            }
     print(f'input file: {input}')
     with open(input) as file:
-        input = file.read()
+        mid = parse(file.read())
         for lang in languages.split(','):
-            print(parse(input, lang))
+            print(f'\n{"-"*40}\ngenerating for {lang}:')
+            print(GENERATORS[lang](mid))
 
 
 if __name__ == '__main__':
