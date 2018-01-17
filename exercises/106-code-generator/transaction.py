ce840beecc595baadcd93f4bb0a223ecd3eff9f2
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2018-01-17 14:33:35 -0200

    renaming files. close file to generate

--- exercises/106-code-generator/transaction.py
+++ exercises/106-code-generator/transaction.py
@@ -1,7 +1,8 @@
 '''Database transaction module.'''
 from carl import command
 
-from languages import generate_cpp, generate_py, parse
+from generators import generate_cpp, generate_py
+from parser import parse
 
 
 @command
@@ -19,9 +20,9 @@ def gen(input: str,
     print(f'input file: {input}')
     with open(input) as file:
         mid = parse(file.read())
-        for lang in languages.split(','):
-            print(f'\n{"-"*40}\ngenerating for {lang}:')
-            print(GENERATORS[lang](mid))
+    for lang in languages.split(','):
+        print(f'\n{"-"*40}\ngenerating for {lang}...')
+        print(GENERATORS[lang](mid))
 
 
 if __name__ == '__main__':
